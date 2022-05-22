3fc52f63f4e5685ef82687b804e532b9764dd0b5
Othmane <othmane.tazi.1997@gmail.com>
2022-05-22 16:02:50 +0200

    feat(db): link db with typeOrm

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: configService.get<'sqlite' | 'mysql'>('DB_TYPE') || 'sqlite',
+        database:
+          configService.get<string>('DB_PATH') || 'data/DATABASE.sqlite',
+        synchronise: false,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
